471a87a99a75ad54f65e16f77eb5dd5c288a1096
yksen <selegakamil@gmail.com>
2022-03-31 14:13:53 +0200

    Lista 2. Zadanie 3.

--- Lista2/zad3.kt
+++ Lista2/zad3.kt
@@ -1,6 +1,9 @@
+import java.math.BigInteger
 fun isCyclic(i: String): Boolean {
+    // if (i.toIntOrNull() == null) return false
     for (j in 2..i.length) {
-        var multiple: String = (j * i.toInt()).toString()
+        var multiple: String = (i.toBigInteger().times(j.toBigInteger())).toString()
+        if (multiple.length < i.length) multiple = "0" + multiple
         for (char in i) {
             val index = multiple.indexOf(char)
             if (index == -1) return false
@@ -11,5 +14,5 @@ fun isCyclic(i: String): Boolean {
 }
 
 fun main() {
-    println(isCyclic("142857"))
+    println(isCyclic("021276595744680851063829787234042531914893617"))
 }
