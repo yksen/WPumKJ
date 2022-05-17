bab99feb2a448b582adcc16373e38ecd443d1769
yksen <selegakamil@gmail.com>
2022-05-17 16:04:37 +0200

    Lista 6.

--- Lista6/lista6.kt
+++ Lista6/lista6.kt
@@ -6,13 +6,13 @@ class Game {
 
     init {
         board = Board()
-        players.add(Player(typeInput(1), symbolInput()))
-        players.add(Player(typeInput(2), if (players[0].symbol == "X") "O" else "X"))
+        players.add(classFromType(typeInput(1), symbolInput()))
+        players.add(classFromType(typeInput(2), if (players[0].symbol == "X") "O" else "X"))
         currentPlayerIndex = (0..1).random()
         currentPlayer = players[currentPlayerIndex]
 
         board.printBoardWithIndexes()
-        move()
+        currentPlayer.move()
     }
 
     fun typeInput(num: Int): String {
@@ -24,6 +24,15 @@ class Game {
         return type
     }
 
+    fun classFromType(type: String, symbol: String): Player {
+        return when (type) {
+            "H" -> HumanPlayer(symbol)
+            "C" -> AiPlayer(symbol)
+            "R" -> RandomPlayer(symbol)
+            else -> Player(symbol)
+        }
+    }
+
     fun symbolInput(): String {
         var symbol: String
         do {
@@ -32,72 +41,74 @@ class Game {
         } while (symbol.length != 1 || !(symbol == "X" || symbol == "O"))
         return symbol
     }
-
-    fun move() {
-        var move: String
-        do {
-            println("${currentPlayer.symbol}'s move: (1) to (9)")
-            move = readLine()!!.uppercase()
-        } while (move.length != 1 ||
-                move.toInt() < 1 ||
-                move.toInt() > 9 ||
-                board.grid[move.toInt() - 1] != "-")
-
-        board.grid[move.toInt() - 1] = currentPlayer.symbol
-        board.printBoard()
-        currentPlayerIndex = (currentPlayerIndex + 1) % 2
-        currentPlayer = players[currentPlayerIndex]
-
-        if (board.checkWin() == -1) {
-            move()
-        }
-        else {
-            println("Game has ended")
-        }
-    }
 }
 
 class Board {
     var grid: MutableList<String> = mutableListOf("-", "-", "-", "-", "-", "-", "-", "-", "-")
 
     fun printBoardWithIndexes() {
-        println("1   2   3")
+        println("\n1   2   3")
         println("4   5   6")
         println("7   8   9")
     }
 
     fun printBoard() {
-        for (i in 0..8) {
-            print(grid[i])
-            if (i % 3 == 2) {
-                println()
-            }
+        println()
+        for (i in 0 until 3) {
+            println(grid[i * 3] + "   " + grid[i * 3 + 1] + "   " + grid[i * 3 + 2])
         }
     }
 
     fun checkWin(): Int {
         for (i in 0..2) {
-            if (grid[i * 3] == grid[i * 3 + 1] && grid[i * 3] == grid[i * 3 + 2]) {
-            
-            }
+            if (grid[i * 3] == grid[i * 3 + 1] && grid[i * 3] == grid[i * 3 + 2]) {}
         }
+        return 0
     }
 }
 
-class Player(_type: String, _symbol: String) {
-    var type: String
+open class Player(_symbol: String) {
     var symbol: String
 
     init {
-        if (_type == "R") {
-            this.type = listOf("H", "C").random()
-        } else {
-            this.type = _type
-        }
         this.symbol = _symbol
+    }
+
+    open fun move() {}
+}
+
+class HumanPlayer(_symbol: String) : Player(_symbol) {
+    override fun move() {
+        // var move: String
+        // do {
+        //     println("${this.symbol}'s move: (1) to (9)")
+        //     move = readLine()!!.uppercase()
+        // } while (move.length != 1 ||
+        //         move.toInt() < 1 ||
+        //         move.toInt() > 9 ||
+        //         board.grid[move.toInt() - 1] != "-")
+
+        // board.grid[move.toInt() - 1] = this.symbol
+        // board.printBoard()
+        // currentPlayerIndex = (currentPlayerIndex + 1) % 2
+        // currentPlayer = players[currentPlayerIndex]
+
+        // if (board.checkWin() == -1) {
+        //     move()
+        // } else {
+        //     println("Game has ended")
+        // }
     }
 }
 
+class RandomPlayer(_symbol: String) : Player(_symbol) {
+    override fun move() {}
+}
+
+class AiPlayer(_symbol: String) : Player(_symbol) {
+    override fun move() {}
+}
+
 fun main() {
-    val game: Game = Game()
+    Game()
 }
