ad78a5fd0cf6345a00faf0b7187df4349f571a07
yksen <selegakamil@gmail.com>
2022-06-21 22:26:36 +0200

    Lista 7. Zadanie 3.

--- Lista7/zad3.kt
+++ Lista7/zad3.kt
@@ -1,34 +1,45 @@
 import java.util.concurrent.*
 
-internal class Product(var from: Long, var to: Long) : Callable<BigInteger> {
-    var localProduct: BigInteger = BigInteger.ONE
-    override fun call(): BigInteger {
-        for (i in from..to) localProduct = localProduct.multiply(BigInteger.valueOf(i))
-        return localProduct
+internal class DivisorCount(var from: Int, var to: Int) : Callable<Pair<Int, Int>> {
+    override fun call(): Pair<Int, Int> {
+        var number: Int = 0
+        var highestCount: Int = 0
+        for (i in from..to) {
+            var count: Int = 1
+            for (j in 1..i / 2) if (i % j == 0) ++count
+            if (count > highestCount) {
+                highestCount = count
+                number = i
+            }
+        }
+        return Pair<Int, Int>(number, highestCount)
     }
 }
 
 fun main() {
     val NUM_THREADS = 10
-    val n: Long = 100000L
+    val n: Int = 100000
     var number: Int = 0
     var highestCount: Int = 0
 
     val executorService = Executors.newFixedThreadPool(NUM_THREADS)
-    val multiplicationTasks: MutableList<Future<BigInteger>> = ArrayList()
-    val taskNum: Long = Math.ceil(n / NUM_THREADS.toDouble()).toLong()
+    val countingTasks: MutableList<Future<Pair<Int, Int>>> = ArrayList()
+    val taskNum: Int = Math.ceil(n / NUM_THREADS.toDouble()).toInt()
 
     for (i in 0 until NUM_THREADS) {
         val fromInInnerRange = taskNum * i + 1
         val toInInnerRange = if (taskNum * (i + 1) > n) n else taskNum * (i + 1)
-        val multiplicationTask: Callable<BigInteger> = Product(fromInInnerRange, toInInnerRange)
-        val futureProduct = executorService.submit(multiplicationTask)
-        multiplicationTasks.add(futureProduct)
+        val countingTask: Callable<Pair<Int, Int>> = DivisorCount(fromInInnerRange, toInInnerRange)
+        val futureCount = executorService.submit(countingTask)
+        countingTasks.add(futureCount)
     }
 
-    for (partialProduct in multiplicationTasks) {
+    for (count in countingTasks) {
         try {
-            calculatedProduct = calculatedProduct.multiply(partialProduct.get())
+            if (count.get().second > highestCount) {
+                number = count.get().first
+                highestCount = count.get().second
+            }
         } catch (exception: CancellationException) {
             exception.printStackTrace()
         } catch (exception: ExecutionException) {
